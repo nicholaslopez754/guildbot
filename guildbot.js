ae487b1a150e41317cfbaf47c968c7650d7a58b2
Nicholas Lopez <nicholaslopez754@gmail.com>
2018-02-02 19:31:27 -0800

    handle dropped connections

--- guildbot.js
+++ guildbot.js
@@ -8,8 +8,34 @@ const AsciiTable = require('ascii-table');
 // Client instance
 const client = new Discord.Client();
 
+// Handle re-connections
+function initializeConnection(config) {
+    function addDisconnectHandler(connection) {
+        connection.on("error", function (error) {
+            if (error instanceof Error) {
+                if (error.code === "PROTOCOL_CONNECTION_LOST") {
+                    console.error(error.stack);
+                    console.log("Lost connection. Reconnecting...");
+
+                    initializeConnection(connection.config);
+                } else if (error.fatal) {
+                    throw error;
+                }
+            }
+        });
+    }
+
+    let connection = mysql.createConnection(config);
+
+    // Add handlers.
+    addDisconnectHandler(connection);
+
+    connection.connect();
+    return connection;
+}
+
 // DB instance
-const connection = mysql.createConnection({
+const connection = initializeConnection({
   host     : process.env.MYSQL_HOST,
   user     : process.env.MYSQL_USER,
   password : process.env.MYSQL_PASSWORD,
