544c6e55d8c71b0e0a8178e1c7bebbdd98074ea9
Nicholas Lopez <nicholaslopez754@gmail.com>
2018-02-04 16:36:13 -0800

    adding more build config

--- guildbot.js
+++ guildbot.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
   host     : process.env.MYSQL_HOST,
   user     : process.env.MYSQL_USER,
   password : process.env.MYSQL_PASSWORD,
-  database : 'guild_db'
+  database : process.env.GUILD_DB
 });
 
 //  Start up
